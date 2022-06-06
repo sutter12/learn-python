a9fbe6482ef9a4fef4b90bb0b952f00843226d6c
Alex Sutter <46454849+sutter12@users.noreply.github.com>
2022-06-06 18:47:44 -0400

    Updated pieChart.py

--- Matplotlib/advancedCharting/pieChart.py
+++ Matplotlib/advancedCharting/pieChart.py
@@ -5,6 +5,39 @@
 import matplotlib.pyplot as plt #should work once package installed
 import numpy as np #should already be installed
 
+data = [
+    {
+        "Label": 'Dr Strange Multiverse of Madness',
+        "Rotten Tomatoes": 74,
+        "color": 'red',
+        "explode": 0
+    },
+    {
+        "Label": 'Spiderman No Way Home',
+        "Rotten Tomatoes": 93,
+        "color": 'blue',
+        "explode": 0
+    },
+    {
+        "Label": 'Eternals',
+        "Rotten Tomatoes": 47,
+        "color": 'teal',
+        "explode": 0
+    },
+    {
+        "Label": 'Shang Chi',
+        "Rotten Tomatoes": 91,
+        "color": 'gold',
+        "explode": 0
+    },
+    {
+        "Label": 'Black Widow',
+        "Rotten Tomatoes": 79,
+        "color": 'black',
+        "explode": 0
+    }
+]
+
 rottenTomatoes = [74 , 93 , 47 , 91, 79]
 list = np.array(rottenTomatoes)
 
