c776e0f111e6128aba50e6ec8dd9708550302e25
Alex Sutter <46454849+sutter12@users.noreply.github.com>
2022-06-06 23:29:25 -0400

    Update pieChart.py

--- Matplotlib/advancedCharting/pieChart.py
+++ Matplotlib/advancedCharting/pieChart.py
@@ -5,6 +5,13 @@
 import matplotlib.pyplot as plt #should work once package installed
 import numpy as np #should already be installed
 
+def getListFromData(key, data):
+    list = []
+    for set in data:
+        value = set[key]
+        list.append(value)
+    return list
+
 data = [
     {
         "Label": 'Dr Strange Multiverse of Madness',
@@ -36,15 +43,19 @@ data = [
         "color": 'black',
         "explode": 0
     }
-]
+]#end data
 
-rottenTomatoes = [74 , 93 , 47 , 91, 79]
-list = np.array(rottenTomatoes)
+# rottenTomatoes = [74 , 93 , 47 , 91, 79]
+rottenTomatoes = getListFromData("Rotten Tomatoes", data)
+values = np.array(rottenTomatoes)
 
-mylabels = ['Dr Strange Multiverse of Madness', 'Spiderman No Way Home', 'Eternals', 'Shang Chi' , 'Black Widow']
-myexplode = [0.2,0,0,0,0]
-mycolors = ['red','blue','teal','gold','black']
+# mylabels = ['Dr Strange Multiverse of Madness', 'Spiderman No Way Home', 'Eternals', 'Shang Chi' , 'Black Widow']
+myLabels = getListFromData("Label", data)
+# myexplode = [0.2,0,0,0,0]
+myExplode = getListFromData("explode", data)
+# myColors = ['red','blue','teal','gold','black']
+myColors = getListFromData("color", data)
 
-plt.pie(list, labels = mylabels, counterclock = False, startangle = 180, explode = myexplode, shadow = True, colors = mycolors)
+plt.pie(values, labels = myLabels, counterclock = False, startangle = 180, explode = myExplode, shadow = True, colors = myColors)
 plt.legend(title = "Movies:")
 plt.show()
