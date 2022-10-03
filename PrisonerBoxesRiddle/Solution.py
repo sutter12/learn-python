a4de32671477043b37282ca06d8772682e5561ba
Alex Sutter <suttera12@gmail.com>
2022-10-03 16:56:26 -0400

    Create Solution.py

---
+++ PrisonerBoxesRiddle/Solution.py
@@ -0,0 +1,21 @@
+# Author: Alexander Sutter
+# Date: 09/28/2022
+# Latest Update: 
+
+# Solution.py
+
+# Solution for the prisoners box riddle
+
+# Imports
+import random
+
+# Setup boxes - each element in list is a box containing a unique number between 1 and 100
+boxes = []
+for x in range(1, 101):
+    num = random.randint(1, 100)
+    
+    while num not in 
+        
+    num = random.randint(1, 100)
+
+print (boxes)
